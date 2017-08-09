7bf53e61d4d85ea5ea408b33e0ac68c076a18274
nikunjgoel95 <nikunjgoel95@gmail.com>
2017-08-09 21:36:34 +0530

    Login Screen Done no changes required!

--- app/src/main/java/com/android/nikunj/shopave/login_activity.java
+++ app/src/main/java/com/android/nikunj/shopave/login_activity.java
@@ -4,22 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class login_activity extends AppCompatActivity {
 
-    private Button mSignup;
+    private TextView mSignup;
+    private TextView mSignin;
     private TextView mSkip;
+    private View signin_view;
+    private View signup_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
-
-        mSignup = (Button) findViewById(R.id.signup);
+        mSignin = (TextView) findViewById(R.id.signin);
+        signin_view = (View) findViewById(R.id.signin_view);
+        signup_view = (View) findViewById(R.id.signup_view);
+        mSignup = (TextView) findViewById(R.id.signup);
         mSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                signin_view.setVisibility(View.INVISIBLE);
+                signup_view.setVisibility(View.VISIBLE);
+                mSignin.setTextColor(getResources().getColor(R.color.signincolor));
+                mSignup.setTextColor(getResources().getColor(R.color.textcolorhigh));
                 Intent i = new Intent(login_activity.this,sign_up.class);
                 startActivity(i);
             }
@@ -32,5 +40,15 @@ public class login_activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        signin_view.setVisibility(View.VISIBLE);
+        signup_view.setVisibility(View.INVISIBLE);
+        mSignin.setTextColor(getResources().getColor(R.color.textcolorhigh));
+        mSignup.setTextColor(getResources().getColor(R.color.signincolor));
     }
 }
