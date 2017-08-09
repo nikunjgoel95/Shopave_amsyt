cfc8570e955a31cc1f2e13b74d868e4b5a3f3a68
nikunjgoel95 <nikunjgoel95@gmail.com>
2017-08-09 18:13:21 +0530

    Added FAB

--- app/src/main/java/com/android/nikunj/shopave/login_activity.java
+++ app/src/main/java/com/android/nikunj/shopave/login_activity.java
@@ -1,13 +1,36 @@
 package com.android.nikunj.shopave;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login_activity extends AppCompatActivity {
 
+    private Button mSignup;
+    private TextView mSkip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        mSignup = (Button) findViewById(R.id.signup);
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(login_activity.this,sign_up.class);
+                startActivity(i);
+            }
+        });
+        mSkip = (TextView) findViewById(R.id.skip);
+        mSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_activity.this,MarketActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
